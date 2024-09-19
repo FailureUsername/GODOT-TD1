11801de5e4e612807bf4e23e1330ea913031e169
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2024-09-19 17:29:35 -0400

    Background, Collisions, Sprite Flip & Animations (Idle & Run)

--- Project/player.gd
+++ Project/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 130.0
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Update
 func _process(delta: float) -> void:
@@ -13,8 +15,24 @@ func _physics_process(delta: float) -> void:
 	pass
 
 func get_input():
+	# Get the direction
 	var direction = Input.get_vector("left", "right", "up", "down")
-	#print(direction)
+	
+	# Visual
+		# Animation
+	if direction : 
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("idle")
+	
+		# Direction facing
+	if direction.x :
+		if direction.x > 0 :
+			animated_sprite.flip_h = false
+		else :
+			animated_sprite.flip_h = true
+	
+	# Apply velocity for movement
 	if direction != Vector2.ZERO :
 		velocity = direction * SPEED
 	else:
